1d651d25b91bda01b9092da24ef63631e0cf968d
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-11 16:44:32 -0500

    Navbar fine, footer not

---
+++ src/main/java/com/matthew/websitehowto/WebsiteHowToApplication.java
@@ -0,0 +1,13 @@
+package com.matthew.websitehowto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteHowToApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteHowToApplication.class, args);
+    }
+
+}
